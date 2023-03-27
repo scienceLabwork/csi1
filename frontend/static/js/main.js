151a08bb7d6135cbbf7f4d1f9373011f154d1f4f
Rudra Shah <heysiri876@gmail.com>
2023-03-28 00:35:40 +0530

    p2

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,103 +1,35 @@
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext('2d');
+let timeline = document.getElementById("timeline");
+let timeline2 = document.getElementById("timeline2");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-var stars = [], // Array that contains the stars
-    FPS = 60, // Frames per second
-    x = 100, // Number of stars
-    mouse = {
-      x: 0,
-      y: 0
-    };  // mouse location
+let day1 = document.getElementById("day1");
+let day2 = document.getElementById("day2");
 
-// Push stars to array
 
-for (var i = 0; i < x; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 1 + 1,
-    vx: Math.floor(Math.random() * 50) - 25,
-    vy: Math.floor(Math.random() * 50) - 25
-  });
-}
 
-// Draw the scene
-
-function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  
-  ctx.globalCompositeOperation = "lighter";
-  
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-  
-    ctx.fillStyle = "#fff";
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = 'black';
-    ctx.stroke();
-  }
-  
-  ctx.beginPath();
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var starI = stars[i];
-    ctx.moveTo(starI.x,starI.y); 
-    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-    for (var j = 0, x = stars.length; j < x; j++) {
-      var starII = stars[j];
-      if(distance(starI, starII) < 150) {
-        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-        ctx.lineTo(starII.x,starII.y); 
-      }
+day1.addEventListener("click", function() {
+    if (timeline.style.display === "none") {
+        timeline.style.display = "block";
+        timeline2.style.display = "none";
+        day1.classList.add("active");
+        day2.classList.remove("active");
+    } else {
+        timeline.style.display = "block";
+        timeline2.style.display = "none";
+        day1.classList.add("active");
+        day2.classList.remove("active");
     }
-  }
-  ctx.lineWidth = 0.05;
-  ctx.strokeStyle = 'white';
-  ctx.stroke();
-}
-
-function distance( point1, point2 ){
-  var xs = 0;
-  var ys = 0;
- 
-  xs = point2.x - point1.x;
-  xs = xs * xs;
- 
-  ys = point2.y - point1.y;
-  ys = ys * ys;
- 
-  return Math.sqrt( xs + ys );
-}
-
-// Update star locations
-
-function update() {
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-  
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
-    
-    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-  }
-}
-
-canvas.addEventListener('mousemove', function(e){
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
 });
-
-// Update and draw
-
-function tick() {
-  draw();
-  update();
-  requestAnimationFrame(tick);
-}
-
-tick();
+day2.addEventListener("click", function() {
+    if (timeline2.style.display === "none") {
+        timeline2.style.display = "block";
+        timeline.style.display = "none";
+        day2.classList.add("active");
+        day1.classList.remove("active");
+    } else {
+        timeline2.style.display = "block";
+        timeline.style.display = "none";
+        day2.classList.add("active");
+        day1.classList.remove("active");
+    }
+});
