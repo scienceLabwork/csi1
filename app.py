f14ee7c5a18eebbdb71af7e072bd87a339dae9a6
Rudra Shah <heysiri876@gmail.com>
2023-03-28 15:21:59 +0530

    404 added

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ port = int(os.environ.get("PORT", 5454))
 def index():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('404.html'), 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
